24a4044e20f3201fbdd5ba66eef2711a014594f9
fWh6qNbXvK <sps1959@ac.eap.gr>
2024-02-29 11:46:09 +0200

    Project code with done tasks commit1

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,25 +4,130 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+let loggedInUser;
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  //Mavredakis write code to check is the username is valid
+  let validusername = users.filter((user)=>{
+    return (user.username === username)// && user.password === password)
+  });
+  if(validusername.length > 0){
+    console.log("Valid username");
+    return true;
+  } else {
+    console.log("Invalid username");
+    return false;
+  }
+}
+//Mavredakis TODO check how to apply filterIndex on Objects instead of Arrays
+const doesExist = (username, isbn)=>{
+  for (const [key, value] of Object.entries(books)) {
+    let userswithsamename = Object.values(books[key].reviews).filter((user)=>{
+      //console.log("username " + username + " user.username " + user.username);      
+      return user.username === username && isbn === key
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  //Mavredakis write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user)=>{
+    
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    console.log("Username & pass correct");    
+    return true;
+  } else {
+    console.log("Username & pass correct");
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task7 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+    }
+    loggedInUser = username;
+    return res.status(200).send(`User ${username} successfully logged in`);
+  }
+  else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task8 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const currentUser = req.user;
+  console.log(currentUser);     
+  const isbn = req.params.isbn;
+  let  isbntomodify = books[isbn]
+  //let username = req.body.username;
+  let review = req.body.review;
+  
+  if (isbntomodify) {  
+                
+      for (const [key, value] of Object.entries(books)) {
+        if (value.reviews) {         
+          
+          const newReview = {
+            username: loggedInUser,//.session.authenticatedUser,
+            review: review            
+          };          
+
+          if (!doesExist(loggedInUser, isbn)){
+            //books[key].reviews = newReview;
+            books[isbn].reviews = newReview;
+            res.send(`Review for ISBN  ${isbn} added by user ${loggedInUser}.`);            
+          }
+          else {
+            let index = books[key].reviews.indexOf(loggedInUser);
+            books[isbn].reviews.splice(index,1);
+            books[isbn].reviews = newReview;
+            //books[key].reviews.splice(index,1);
+            //books[key].reviews = newReview;
+            res.send(`Review for ISBN  ${isbn} updated by user ${loggedInUser}.`);            
+          }             
+        }        
+      }
+  }
+  else{
+      res.send("Unable to find ISBN!");
+  }
+  
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Mavredakis for Task9
+  const isbn = req.params.isbn;
+  //const user = loggedInUser;
+  if (isbn && books[isbn].reviews.username === loggedInUser){
+    delete books[isbn].reviews.review
+  }
+  res.send(`Book review for isbn with the isbn  ${isbn} deleted.`);
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,161 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+//Mavredakis for Task10
+const axios = require('axios').default;
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task6 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    }
+    else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  }
+  else
+    return res.status(404).json({message: "Please provide password as well as username!"});   
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task1 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4))  
 });
+
+//Mavredakis for Task10
+const getBooksData = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    // Handle response
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+};
+getBooksData();
+
+//Mavredakis for Task11
+const getBooksDataByISBN = async (isbn) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    // Handle response
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+};
+getBooksDataByISBN(3);
+
+//Mavredakis for Task12
+const getBooksDataByAuthor = async (author) => {
+  //console.log(author);
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    // Handle response
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+};
+getBooksDataByAuthor("Chinua Achebe");
+
+//Mavredakis for Task13
+const getBooksDataByTitle = async (title) => {
+  //console.log(title);
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    // Handle response
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+};
+getBooksDataByTitle("Fairy tales");
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task2 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task3 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;    
+  
+  //for (let i = 1; i <= 10; i++) {
+  for (const [key, value] of Object.entries(books)) {
+    //const book = books[i];    
+    const book = books[key];     
+    if (author === book.author){      
+      //console.log(`Book ${i}: ${book.title} by ${book.author}`);
+      //res.send(books[i]);
+      res.send(books[key]);
+    }       
+  }
+  res.send("Author does not exist");    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  //Mavredakis Task4 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  
+  for (const [key, value] of Object.entries(books)) {
+    //const book = books[i];
+    const book = books[key];    
+    if (title === book.title)
+      //console.log(`Book ${i}: ${book.title} by ${book.author}`);
+      //res.send(books[i]);
+      res.send(books[key]);
+      //break;
+    //else
+      //res.send("Title does not exist");    
+  }
+  res.send("Title does not exist"); 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Mavredakis Task5 Write your code here
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;  
+  for (const [key, value] of Object.entries(books)) {
+    if (key === isbn){
+      //console.log(`Book ${i}: ${book.title} by ${book.author}`);      
+      res.send(value.reviews);
+    }
+  }
+  res.send("ISBN does not exist");
 });
 
 module.exports.general = public_users;
